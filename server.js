98cdc9ba0773500acad2afc55493de8fe6cf6965
gustim <augustobormoreira@gmail.com>
2025-06-22 19:23:46 -0300

    Changes made to server.js

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
-import express from 'express';
+import express from 'express'
 
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+    users.push(req.body)
+})
+
+app.get('/usuarios', (req, res) => {
+    res.send(users)
+})
+
+
+app.listen(3000);
